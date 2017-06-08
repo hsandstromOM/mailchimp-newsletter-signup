dc41299cec25cf77e8bd5adebc9d98dc7a153529
hsandstromOM <hosea@obviouslee.com>
2017-06-08 11:58:04 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//Adapted from https://www.codementor.io/mattgoldspink/integrate-mailchimp-with-nodejs-app-du10854xp
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var request = require('superagent');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('views'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var mailchimpInstance   = 'us16',
+    listUniqueId        = '352a183a71',
+    mailchimpApiKey     = '2b924b3569970fbac04b393fbc8294cf-us16';
+
+app.post('/signup', function (req, res) {
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+          'email_address': req.body.email,
+          'status': 'subscribed',
+          'merge_fields': {
+            'FNAME': req.body.firstName,
+            'LNAME': req.body.lastName,
+            'MESSAGE': req.body.message
+          }
+        })
+            .end(function(err, response) {
+              if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+                return res.redirect('/thankyou.html');
+              } else {
+                res.send('Sign Up Failed :(');
+              }
+          });
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
