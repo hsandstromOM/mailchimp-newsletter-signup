d282fc45902f0cbe04e2d57a6ea8cdad9f7d1ce3
hsandstromOM <hosea@obviouslee.com>
2017-06-08 13:40:26 -0400

    Added .env and tested

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var request = require('superagent');
+require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('views'));
@@ -12,15 +13,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var mailchimpInstance   = 'us16',
-    listUniqueId        = '352a183a71',
-    mailchimpApiKey     = '2b924b3569970fbac04b393fbc8294cf-us16';
+// var mailchimpInstance   = 'us16',
+//     listUniqueId        = '352a183a71',
+//     mailchimpApiKey     = '2b924b3569970fbac04b393fbc8294cf-us16';
 
 app.post('/signup', function (req, res) {
     request
-        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .post('https://' + process.env.DB_mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + process.env.DB_listUniqueId + '/members/')
         .set('Content-Type', 'application/json;charset=utf-8')
-        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .set('Authorization', 'Basic ' + new Buffer('any:' + process.env.DB_mailchimpApiKey ).toString('base64'))
         .send({
           'email_address': req.body.email,
           'status': 'subscribed',
